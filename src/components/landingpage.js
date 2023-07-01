065c13713a780c4d85a22b5c7ac26b9af7cb203a
MaliceKy <129139550+MaliceKy@users.noreply.github.com>
2023-07-01 17:21:51 -0400

    Animations are all working for all clickables

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,80 +1,82 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import "../assests/styles/LandingPage.css";
 import { ReactComponent as LinkedInIcon } from '../assests/images/LinkedIn.svg';
 import { ReactComponent as GitHubIcon } from '../assests/images/Github.svg';
 import { ReactComponent as ArrowRightIcon } from '../assests/images/angle-small-right.svg';
 
-
 const LandingPage = () => {
+  const [clicked, setClicked] = useState({});
+  const [redirect, setRedirect] = useState(null);
 
-  const handleClick = (e) => {
+  const handleSocialClick = (e, href, social) => {
     e.preventDefault();
-
-    // Add the 'clicked' class to the clicked element
-    e.currentTarget.classList.add('clicked');
-
-    // Check if the SVG path exists
-    const svgPath = e.currentTarget.querySelector('.arrow path');
-    if (svgPath) {
-        svgPath.classList.add('clicked');
-    }
-
-    // Remove the class after the animation duration (in this case, 0.6s)
-    setTimeout(() => {
-      e.currentTarget.classList.remove('clicked');
-      if (svgPath) {
-          svgPath.classList.remove('clicked');
-      }
-    }, 600);
+    setClicked(prev => ({ ...prev, [social]: true }));
+    setRedirect(href);
   };
 
+  const handleButtonClick = (e, button, text) => {
+    e.preventDefault();
+    setClicked(prev => ({ ...prev, [button]: true, [text]: true }));
+    // Code for handling button click goes here
+  };
 
+  useEffect(() => {
+    if (redirect) {
+      setTimeout(() => {
+        window.open(redirect, '_blank');
+        setClicked({});
+        setRedirect(null);
+      }, 600);
+    } else if (Object.keys(clicked).length > 0) {
+      setTimeout(() => {
+        setClicked({});
+      }, 600);
+    }
+  }, [redirect, clicked]);
 
   return (
-    <div className="background"> {/* Background */}
+    <div className="background">
       <div className="container position-relative">
-        {/* 'Hello I'm' for small screens */}
         <div className="hello-container position-absolute w-100 d-block d-md-none">
           <div className="hello">Hello I'm</div>
         </div>
-        {/* 'Hello I'm' for larger screens */}
         <div className="hello-container d-none d-md-block">
           <div className="hello">Hello I'm</div>
         </div>
-        <div className="main-content position-relative"> {/* Main Contents */}
+        <div className="main-content position-relative">
           <div className="content-container">
             <h1 className="Title">Kyle Malice</h1>
             <p className="Title-Desc">Web development enthusiast, proficient in JavaScript, CSS, HTML, and React, pursuing a Bachelor's in Computer Science. Ambitious, challenge-seeking, and eager to innovate in the dynamic web industry. Simple, direct, future-focused. Let's connect.</p>
-            <row>
+            <div className="row">
               <div className="col-12">
-                <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
-                  <LinkedInIcon onClick={handleClick} className="social-icon linkedin-icon" />
+                <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer" onClick={(e) => handleSocialClick(e, 'https://www.linkedin.com', 'linkedin')}>
+                  <LinkedInIcon className={`social-icon linkedin-icon ${clicked['linkedin'] ? 'clicked' : ''}`} />
                 </a>
-                <a href="https://www.github.com" target="_blank" rel="noopener noreferrer">
-                  <GitHubIcon onClick={handleClick} className="social-icon github-icon" />
+                <a href="https://www.github.com" target="_blank" rel="noopener noreferrer" onClick={(e) => handleSocialClick(e, 'https://www.github.com', 'github')}>
+                  <GitHubIcon className={`social-icon github-icon ${clicked['github'] ? 'clicked' : ''}`} />
                 </a>
               </div>
-            </row>
-            <div className="buttonContainer"> {/* Buttons */}
-              <button href="#" className="tabs" onClick={handleClick}>
-                More About Me.
-                <ArrowRightIcon className="arrow" />
-              </button>
-              <button href="#" className="tabs" onClick={handleClick}>
-                Past Projects.
-                <ArrowRightIcon className="arrow" />
-              </button>
-              <button href="#" className="tabs" onClick={handleClick}>
-                Contact.
-                <ArrowRightIcon className="arrow" />
-              </button>
+            </div>
+            <div className="buttonContainer">
+            <button href="#" className={`tabs ${clicked['about'] ? 'clicked' : ''}`} onClick={(e) => handleButtonClick(e, 'aboutButton', 'aboutText')}>
+              <span className={`button-text ${clicked['aboutText'] ? 'clicked' : ''}`}>More About Me.</span>
+              <ArrowRightIcon className={`arrow ${clicked['aboutButton'] ? 'clicked' : ''}`} />
+            </button>
+            <button href="#" className={`tabs ${clicked['projects'] ? 'clicked' : ''}`} onClick={(e) => handleButtonClick(e, 'projectsButton', 'projectsText')}>
+              <span className={`button-text ${clicked['projectsText'] ? 'clicked' : ''}`}>Past Projects.</span>
+              <ArrowRightIcon className={`arrow ${clicked['projectsButton'] ? 'clicked' : ''}`} />
+            </button>
+            <button href="#" className={`tabs ${clicked['contact'] ? 'clicked' : ''}`} onClick={(e) => handleButtonClick(e, 'contactButton', 'contactText')}>
+              <span className={`button-text ${clicked['contactText'] ? 'clicked' : ''}`}>Contact.</span>
+              <ArrowRightIcon className={`arrow ${clicked['contactButton'] ? 'clicked' : ''}`} />
+            </button>
+
             </div>
           </div>
         </div>
       </div>
     </div>
-  );  
-    
+  );
 };
-export default LandingPage;
 
+export default LandingPage;
