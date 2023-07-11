fbcc600be1eaf45ab30e054c7d8df8fe48ef627f
MaliceKy <Kyle.Malice2001@gmail.com>
2023-07-11 11:17:20 -0400

    About Me Section Progressed

--- src/components/aboutpage.js
+++ src/components/aboutpage.js
@@ -1,12 +1,25 @@
 // About.js
 // src/components/aboutpage.js
 import React from 'react';
+import '../assests/styles/about.css'
 
 const About = () => {
   return (
-    <div>
-      <h1>About Us</h1>
-      <p>This is the About page for our website. Here we will tell you all about our company and what we do.</p>
+    <div className="about-background">
+        <div className="about-container position-relative">
+            <div className="about-hello-container position-absolute w-100 d-block d-md-none">
+                <div className="about-hello">About Me.</div>
+            </div>
+            <div className="about-hello-container d-none d-md-block">
+                <div className="about-hello">About Me.</div>
+            </div>
+            <div className="about-main-content position-relative">
+                <div className="about-content-container">
+                    <h1 className="about-Title">Introduction.</h1>
+                    <p className="about-Title-Desc">Web development enthusiast, proficient in JavaScript, CSS, HTML, and React, pursuing a Bachelor's in Computer Science. Ambitious, challenge-seeking, and eager to innovate in the dynamic web industry. Simple, direct, future-focused. Let's connect.</p>
+                </div>
+            </div>
+        </div>
     </div>
   );
 };
