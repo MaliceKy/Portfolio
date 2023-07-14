88a9bd012c0a3d8de7040e7cfabc194dfc126e20
MaliceKy <Kyle.Malice2001@gmail.com>
2023-07-14 14:39:50 -0400

    Navbar functional and is a good concept for now

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,25 +1,41 @@
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import '../assests/styles/navbar.css'
+import { Navbar, Nav } from 'react-bootstrap';
+import '../assests/styles/navbar.css';
 
 const NavBar = () => {
-    return (
-        <nav className="navbar">
-            <ul className="navbar-nav">
-                <li className="nav-item">
-                    <Link className="nav-link" to="/">Home</Link>
-                </li>
-                <li className="nav-item">
-                    <Link className="nav-link" to="/about">About</Link>
-                </li>
-                <li className="nav-item">
-                    <Link className="nav-link" to="/projects">Projects</Link>
-                </li>
-                <li className="nav-item">
-                    <a href="mailto:Malice.Kyle@gmail.com" className="nav-link">Contact</a> 
-                </li>
+    const [isOpen, setIsOpen] = useState(false);
 
-            </ul>
-        </nav>
+    const handleToggle = () => setIsOpen(!isOpen);
+
+    return (
+        <Navbar collapseOnSelect expand="lg" variant="dark" className="custom-navbar">
+            <Navbar.Toggle 
+                aria-controls="responsive-navbar-nav" 
+                onClick={handleToggle} 
+                className={`custom-toggler ${isOpen ? 'open' : ''} d-lg-none`}
+            >
+                <div></div>
+                <div></div>
+                <div></div>
+            </Navbar.Toggle>
+            <div className={`mobile-nav ${isOpen ? 'open' : ''} d-lg-none`}>
+                <Nav className="ms-auto">
+                    <Nav.Link as={Link} to="/">Home</Nav.Link>
+                    <Nav.Link as={Link} to="/about">About</Nav.Link>
+                    <Nav.Link as={Link} to="/projects">Projects</Nav.Link>
+                    <Nav.Link href="mailto:Malice.Kyle@gmail.com">Contact</Nav.Link>
+                </Nav>
+            </div>
+            <div className="ms-auto d-none d-lg-block">
+                <Nav>
+                    <Nav.Link as={Link} to="/">Home</Nav.Link>
+                    <Nav.Link as={Link} to="/about">About</Nav.Link>
+                    <Nav.Link as={Link} to="/projects">Projects</Nav.Link>
+                    <Nav.Link href="mailto:Malice.Kyle@gmail.com">Contact</Nav.Link>
+                </Nav>
+            </div>
+        </Navbar>
     );
 };
 
