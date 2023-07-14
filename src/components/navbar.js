d60dc0a65a25d8b336088e6b0b7257cf21e8845c
MaliceKy <Kyle.Malice2001@gmail.com>
2023-07-14 16:23:42 -0400

    Fixed a bug for the large screens navbar functionality

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { Navbar, Nav } from 'react-bootstrap';
 import '../assests/styles/navbar.css';
 
@@ -15,7 +15,7 @@ const NavBar = () => {
             <Navbar.Toggle 
                 aria-controls="responsive-navbar-nav" 
                 onClick={handleToggle} 
-                className={`custom-toggler ${isOpen ? 'open' : ''} d-lg-none`}
+                className={`custom-toggler ${isOpen ? 'open' : ''}`}
             >
                 <div></div>
                 <div></div>
@@ -23,25 +23,26 @@ const NavBar = () => {
             </Navbar.Toggle>
             <div className={`mobile-nav-overlay ${isOpen ? 'open' : ''}`}>
                 <Nav className="overlay-content">
-                    <Nav.Link as={Link} to="/" onClick={handleClose}>Home</Nav.Link>
-                    <Nav.Link as={Link} to="/about" onClick={handleClose}>About</Nav.Link>
-                    <Nav.Link as={Link} to="/projects" onClick={handleClose}>Projects</Nav.Link>
+                    <Nav.Link as={NavLink} to="/" exact onClick={handleClose} activeClassName="active-link">Home</Nav.Link>
+                    <Nav.Link as={NavLink} to="/about" onClick={handleClose} activeClassName="active-link">About</Nav.Link>
+                    <Nav.Link as={NavLink} to="/projects" onClick={handleClose} activeClassName="active-link">Projects</Nav.Link>
                     <Nav.Link href="mailto:Malice.Kyle@gmail.com">Contact</Nav.Link>
                 </Nav>
             </div>
-            <div className="ms-auto d-none d-lg-block">
-                <Nav>
-                    <Nav.Link as={Link} to="/">Home</Nav.Link>
-                    <Nav.Link as={Link} to="/about">About</Nav.Link>
-                    <Nav.Link as={Link} to="/projects">Projects</Nav.Link>
+            <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
+                <Nav className="d-none d-lg-flex">
+                    <Nav.Link as={NavLink} to="/" exact activeClassName="active-link">Home</Nav.Link>
+                    <Nav.Link as={NavLink} to="/about" activeClassName="active-link">About</Nav.Link>
+                    <Nav.Link as={NavLink} to="/projects" activeClassName="active-link">Projects</Nav.Link>
                     <Nav.Link href="mailto:Malice.Kyle@gmail.com">Contact</Nav.Link>
                 </Nav>
-            </div>
+            </Navbar.Collapse>
         </Navbar>
     );
 };
 
 export default NavBar;
+
 
 
 
