b21f67a3b79d3f19b35e0c9dff4f27e99ce8856a
MaliceKy <Kyle.Malice2001@gmail.com>
2023-07-16 15:55:58 -0400

    Added a ton of styling and structure to the extended project pop up

--- src/components/projectspage.js
+++ src/components/projectspage.js
@@ -3,11 +3,13 @@ import React, { useState } from 'react';
 import '../assests/styles/projects.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import VaultWiseGif from '../assests/images/VaultWise.gif'
+import { ReactComponent as ArrowRightIcon } from '../assests/images/angle-small-right.svg';
 
 const projectData = [
   {
     title: 'VaultWise',
     description: 'Personal bankstatement tracker and AI assistant.',
+    descriptionExtended: 'VaultWise is a personal bank statement tracker and emerging AI assistant. Built using React, JavaScript, and CSS, it delivers a seamless experience across devices. Users can securely sign in or explore as guests. Once in, VaultWise analyzes uploaded bank statements, summarizing income, expenses, and transactions. A dynamic graph displays monthly bank balance trends. Under development is an AI toolbox, leveraging GPT 3.5 Turbo, designed to guide users toward financial goals. Ultimately, VaultWise aims to make finance management simpler, insightful, and interactive.',
     image: VaultWiseGif,
     technologies: ['React', 'JavaScript', 'CSS']
   },
@@ -23,17 +25,23 @@ const ExpandedProject = ({ project, onClose, closing }) => {
   if (!project) return null;
 
   return (
-    <div className="container">
-      <div className="row justify-content-center">
-        <div className={`col-12 col-md-5 expanded-project ${closing ? 'closing' : ''}`}>
-          <button className="close-button" onClick={onClose}>X</button>
-          <img src={project.image} alt={project.title} />
-          <h2>{project.title}</h2>
-          <p>{project.description}</p>
-          <div>
-            {project.technologies.map((tech, i) => (
-              <span key={i} className="project-tech">{tech}</span>
-            ))}
+    <div className={`overlay ${closing ? 'closing' : ''}`}>
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className={`col-12 col-md-5 expanded-project ${closing ? 'closing' : ''}`}>
+            <button className="close-button" onClick={onClose}>Projects 
+              <ArrowRightIcon className={`arrow`} />
+            </button>
+            <h2>{project.title}</h2>
+            <img src={project.image} alt={project.title} />
+            <h3>Description:</h3>
+            <p>{project.descriptionExtended}</p>
+            <h3>Technologies:</h3>
+            <div className="projectTechnologiesExt">
+              {project.technologies.map((tech, i) => (
+                <span key={i} className="project-tech">{tech}</span>
+              ))}
+            </div>
           </div>
         </div>
       </div>
@@ -50,7 +58,7 @@ const Projects = () => {
     setTimeout(() => {
       setSelectedProject(null);
       setClosing(false);
-    }, 500); // Delay should be equal to the duration of your CSS transition
+    }, 500);
   };
 
   return (
